788a95f156593edb25973bd183620c5cf8d9e03d
Invizabel <46697708+Invizabel@users.noreply.github.com>
2025-09-26 11:18:14 -0700

    Add files via upload

--- planty.js
+++ planty.js
@@ -10,11 +10,10 @@ let draw_line = ffi("void canvas_draw_line(void*,int,int,int,int)");
 let draw_str = ffi("void canvas_draw_str(void*, int, int, char*)");
 let Math = require("math");
 
-let input_events = furi_record_open("input_events");
-//let input_event = furi_pubsub_subscribe("input_event_queue", 1, null);
+let input_event = furi_record_open("input_events");
+
 let gui = furi_record_open("gui");
 let canvas = gui_direct_draw_acquire(gui);
-
 let running = true;
 let sun = 50;
 
@@ -40,7 +39,7 @@ let plants = [
 
 function calc_sun()
 {
-    if ((Math.floor(Math.random() * 2) + 1) === 1)
+    if ((Math.floor(Math.random() * 9) + 1) === 1)
     {
         sun = Math.min(sun + 25, 999);
     }
@@ -122,3 +121,4 @@ while (running)
 
 gui_direct_draw_release(gui);
 furi_record_close("gui");
+furi_record_close("input_events");
